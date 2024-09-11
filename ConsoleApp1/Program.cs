11a5803d3d5a24b6afb7dd2afa7de22278f58a71
Ali Fattahi Rad <alifr7667@gmail.com>
2024-09-11 15:11:45 +0330

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadKey().KeyChar;
+        var result = (char)(input +1);
+        Console.WriteLine($"\n{result}");
+        Main();
+    }
+}
